06267b79f04fcea5ea41c48f484004427c526c8b
MOK-YH <dudgn3015@naver.com>
2025-01-14 15:59:24 +0900

    Initial commit: Add solution for basic level

---
+++ basic/solution.py
@@ -0,0 +1,14 @@
+# 정수 찾기
+def solution(num_list, n):
+    answer = 0
+    if n in num_list:
+        answer = 1
+    else:
+        answer = 0 
+    return answer
+
+# 테스트 코드
+num_list = [1, 2, 3, 4, 5]
+n = 3
+result = solution(num_list, n)
+print(result)
